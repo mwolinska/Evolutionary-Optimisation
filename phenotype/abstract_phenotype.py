0ac48d89e494ee5f0cd3284ee6218c906c1f634b
Marta <mswolinska@gmail.com>
2022-07-19 17:35:25 +0100

    Add expected value property to Phenotype and spli

--- phenotype/abstract_phenotype.py
+++ phenotype/abstract_phenotype.py
@@ -5,6 +5,25 @@ from genotype.genotype import Genotype
 
 class AbstractPhenotype(ABC):
 
+    def __init__(self):
+        self._expected_value = None
+
     @abstractmethod
     def function_to_optimise(self, genotype: Genotype):
         pass
+
+    @abstractmethod
+    def optimise_for_string_genotype(self, genotype: Genotype):
+        pass
+
+    @property
+    @abstractmethod
+    def expected_value(self):
+        pass
+        # return self._expected_value
+
+    @expected_value.setter
+    @abstractmethod
+    def expected_value(self, exp_value):
+        pass
+        # self._expected_value = exp_value
